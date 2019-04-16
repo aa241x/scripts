4de26ca5c32c430f6bac55d308b1a021e4492f2d
Adrien Perkins <adrienp@stanford.edu>
2019-04-16 14:02:20 -0700

    update the script to fix errors and add additional libraries

--- install_everything.sh
+++ install_everything.sh
@@ -9,8 +9,8 @@
 sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) main universe restricted multiverse"
 
 # updates
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get -y update
+sudo apt-get -y upgrade
 
 
 #
@@ -20,8 +20,8 @@ sudo apt-get upgrade
 # the core install elements
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
-sudo apt update
-sudo apt install ros-melodic-ros-base
+sudo apt-get -y update
+sudo apt-get -y install ros-melodic-ros-base
 
 # initialize and update rosdep
 sudo rosdep init
@@ -32,7 +32,7 @@ echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
 # additional installations for building packages
-sudo apt install python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt-get -y install python-rosinstall python-rosinstall-generator python-wstool build-essential
 
 # create catkin directory and test with a call to make
 mkdir -p ~/catkin_ws/src
@@ -44,14 +44,14 @@ cd ~
 # mavros install
 #
 
-sudo apt-get install ros-melodic-mavros ros-melodic-mavros-extras
+sudo apt-get -y install ros-melodic-mavros ros-melodic-mavros-extras
 
 # download the script to install the geographic lib
 cd ~/Downloads
 wget https://raw.githubusercontent.com/mavlink/mavros/master/mavros/scripts/install_geographiclib_datasets.sh
 
 # make script executable and execute it
-sudo chmod +x install_geographiclib_datasets
+sudo chmod +x install_geographiclib_datasets.sh
 sudo ./install_geographiclib_datasets.sh
 
 
@@ -59,11 +59,11 @@ sudo ./install_geographiclib_datasets.sh
 # additional stuff
 #
 
-# install git and ssh
-sudo apt-get install git openssh-server
+# additional ros things
+sudo apt-get -y install ros-melodic-cv-bridge ros-melodic-image-transport
 
-# install opencv
-sudo apt-get install libopencv-dev
+# install git, ssh, vim, and opencv
+sudo apt-get -y install git openssh-server vim libopencv-dev
 
 # install raspiberry pi cam library
 cd ~
@@ -91,3 +91,6 @@ cd ~/catkin_ws/src
 git clone https://github.com/aa241x/aa241x_student.git
 cd ~/catkin_ws/
 catkin_make
+
+# reboot the system
+sudo reboot
